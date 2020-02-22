576456ef2a5c645bf69b6b2f0e4dda6af7bc247f
Leven <leven.zhang.work@outlook.com>
2020-02-22 20:56:06 +0800

    Bootstrap Grid
    This is my tryouts for Bootstrap Grid System

---
+++ Exercise 2/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Ezra Miller Movies</title>
+  <!-- CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <!-- JS Library: JS, Popper.js, and jQuery -->
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+
+
+<body>
+  <div class="container mt-5">
+  <h2>Ezra Miller: Best Movies</h2>
+    <div class="row">
+
+      <div class="col-12 col-sm-6 col-lg-4 mt-5">
+        <div class="card" style="width: 100%;">
+        <img src="images/The-Perks-of-being-a-Wallflower.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">The Perks of Being a Wallflower</h5>
+          <h6 class="card-subtitle mb-2 text-muted">2012</h6>
+          <p class="card-text">An introvert freshman is taken under the wings of two seniors who welcome him to the real world.</p>
+          <a href="https://www.imdb.com/title/tt1659337/" target="_blank" class="btn btn-primary">Learn More</a>
+        </div>
+        </div>
+      </div>
+
+    <div class="col-12 col-sm-6 col-lg-4 mt-5">
+      <div class="card" style="width: 100%;">
+      <img src="images/we-need-to-talk-about-kevin.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">We Need to Talk About Kevin</h5>
+        <h6 class="card-subtitle mb-2 text-muted">2011</h6>
+        <p class="card-text">Kevin's mother struggles to love her strange child, despite the increasingly dangerous things he says and does as he grows up. But Kevin is just getting started, and his final act will be beyond anything anyone imagined.</p>
+        <a href="https://www.imdb.com/title/tt1242460/" target="_blank" class="btn btn-primary">Learn More</a>
+      </div>
+      </div>
+    </div>
+
+    <div class="col-12 col-sm-6 col-lg-4 mt-5">
+      <div class="card" style="width: 100%;">
+      <img src="images/City-Island.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">City Island</h5>
+        <h6 class="card-subtitle mb-2 text-muted">2009</h6>
+        <p class="card-text">Prison guard Vince tells Molly from acting class, that one inmate is his 24 y.o. love child. Vince takes him home to stay with his family - straight A son with fat girl fetish, college dropout/stripper daughter and cute wife.</p>
+        <a href="https://www.imdb.com/title/tt1174730/" target="_blank" class="btn btn-primary">Learn More</a>
+      </div>
+      </div>
+    </div>
+
+    <div class="col-12 col-sm-6 col-lg-4 mt-5">
+      <div class="card" style="width: 100%;">
+      <img src="images/Busted-Walk.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Busted Walk</h5>
+        <h6 class="card-subtitle mb-2 text-muted">2011</h6>
+        <p class="card-text">Busted for pot in his school locker, a disabled teenage guitar hero (played by Ezra Miller) limps to his uncool punishment - working for his father.</p>
+        <a href="https://www.imdb.com/title/tt1252597/" target="_blank" class="btn btn-primary">Learn More</a>
+      </div>
+      </div>
+    </div>
+
+    <div class="col-12 col-sm-6 col-lg-4 mt-5">
+      <div class="card" style="width: 100%;">
+      <img src="images/fantastic-beasts-and-where-to-find-them.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Fantastic Beasts and Where to Find Them</h5>
+        <h6 class="card-subtitle mb-2 text-muted">2016</h6>
+        <p class="card-text">The adventures of writer Newt Scamander in New York's secret community of witches and wizards seventy years before Harry Potter reads his book in school.</p>
+        <a href="https://www.imdb.com/title/tt1659337/?ref_=nm_knf_i1" target="_blank" class="btn btn-primary">Learn More</a>
+      </div>
+      </div>
+    </div>
+
+    <div class="col-12 col-sm-6 col-lg-4 mt-5">
+      <div class="card" style="width: 100%;">
+      <img src="images/The-Stanford Prison-Experiment.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">The Stanford Prison Experiment</h5>
+        <h6 class="card-subtitle mb-2 text-muted">2015</h6>
+        <p class="card-text">In 1971, twenty-four male students are selected to take on randomly assigned roles of prisoners and guards in a mock prison situated in the basement of the Stanford psychology building.</p>
+        <a href="https://www.imdb.com/title/tt0420293/" target="_blank" class="btn btn-primary">Learn More</a>
+      </div>
+      </div>
+    </div>
+
+
+
+
+  </div><!-- row -->
+</div><!-- content container -->
+
+</body>
+</html>
